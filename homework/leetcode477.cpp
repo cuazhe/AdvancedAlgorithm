3ef04fe7b77363a1ca7d509da66c5d8eaf75b616
cuazhe <34105625+cuazhe@users.noreply.github.com>
2020-11-28 19:58:18 +0800

    Create leetcode477.cpp

---
+++ homework/leetcode477.cpp
@@ -0,0 +1,26 @@
+int totalHammingDistance(vector<int>& nums)
+{
+    if (nums.empty())
+        return 0;
+
+    int ans = 0, n = nums.size();
+    vector<int> cnt(32, 0);         // count of elements with a particular bit ON
+
+    for (auto num : nums) {         // loop over every element
+        int i = 0;
+        while (num > 0) {           // check every bit
+            cnt[i] += (num & 0x1);
+            num >>= 1;
+            i++;
+        }
+    }
+
+    for (auto&& k : cnt) {           // loop over every bit count
+        ans += k * (n - k);
+    }
+
+    return ans;
+}
+
+// from LeetCode
+// https://leetcode-cn.com/problems/total-hamming-distance/solution/yi-ming-ju-chi-zong-he-by-leetcode/
